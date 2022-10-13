99d9a63b8f89c5703335c2c6dea86f7f8568cbd4
s1lencef <98669592+s1lencef@users.noreply.github.com>
2022-10-13 23:49:08 +0300

    Add files via upload

---
+++ FOURTH1.C
@@ -0,0 +1,21 @@
+#include <conio.h>
+#include <stdio.h>
+#include <dos.h>
+
+int main(){
+	int i,j;
+	int *c_x,*c_y;
+	clrscr();
+	window(19,4,61,16);
+	textbackground(1);
+	clrscr();
+	window(20,5,60,15);
+	textbackground(0);
+	clrscr();
+
+
+
+
+
+	return 0;
+}
